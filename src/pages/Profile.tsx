a758f240bf51d404caf0810d5a2bceb0ec6de59c
Sai A <142511747+zer356v@users.noreply.github.com>
2025-10-11 18:53:06 +0530

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -208,7 +208,6 @@ export default function ChemistryLabProfilePage() {
    useEffect(()=>{
       const getScore = async ()=>{
         const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/api/add-experiment/${user.uid}`);
-        console.log(response.data);
         const userExperimentArray = response.data;
         const {score} = userExperimentArray[userExperimentArray.length - 1];
         setScore(score);
